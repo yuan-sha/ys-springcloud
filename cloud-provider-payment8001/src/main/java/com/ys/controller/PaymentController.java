fbdfbfe83fe751501bc47bc31d37c4cbf70a3ddd
185388 <ysha@progrexion.com>
2021-03-29 10:56:34 -0600

    Add Discovery

--- cloud-provider-payment8001/src/main/java/com/ys/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/ys/controller/PaymentController.java
@@ -1,10 +1,12 @@
 package com.ys.controller;
 
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import com.ys.entities.CommonResult;
 import com.ys.entities.Payment;
 import com.ys.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -19,6 +21,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
@@ -44,5 +49,17 @@ public class PaymentController {
 
     public List<Payment> findAll() {
         return paymentService.findAll();
+    }
+
+    @RequestMapping(value = "/payment/discovery", method= RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = discoveryClient.getServices();
+        System.out.println("*********"+list);
+
+        List<ServiceInstance> srvList = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance element:srvList) {
+            System.out.println(element.getServiceId()+"\t"+element.getHost()+"\t"+element.getPort()+"\t"+element.getUri());
+        }
+        return this.discoveryClient;
     }
 }
