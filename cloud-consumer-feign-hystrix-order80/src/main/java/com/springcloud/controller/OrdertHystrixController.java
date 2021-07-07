26df8c81731df3aeb7993c149e2cc5407d3ba608
yuan-sha <yuansha312@gmail.com>
2021-07-07 13:37:14 -0600

    add hystrix

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/springcloud/controller/OrdertHystrixController.java
@@ -0,0 +1,26 @@
+package com.springcloud.controller;
+
+import com.springcloud.service.PaymentHystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrdertHystrixController {
+
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id){
+        return paymentHystrixService.paymentInfo_OK(id);
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    public String paymentInfo_Timeout(@PathVariable("id") Integer id){
+        return paymentHystrixService.paymentInfo_Timeout(id);
+    }
+}
