30a9a86f993f219444cc5beb6dcb323369373f77
yuan-sha <yuansha312@gmail.com>
2021-07-07 09:35:15 -0600

    change project

--- cloud-provider-hystrix-payment8001/src/main/java/com/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/PaymentHystrixMain8001.java
@@ -1,4 +1,4 @@
-package com.springcloud;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class Payment8002 {
+public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
-        SpringApplication.run(Payment8002.class,args);
+        SpringApplication.run(PaymentHystrixMain8001.class,args);
     }
 }
