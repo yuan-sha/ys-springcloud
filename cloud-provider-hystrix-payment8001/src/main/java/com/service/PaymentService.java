eaae6474234cc4e7a02d5d77920dd91253d7c2ca
yuan-sha <yuansha312@gmail.com>
2021-07-07 13:59:14 -0600

    Fallback

--- cloud-provider-hystrix-payment8001/src/main/java/com/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -10,13 +12,20 @@ public class PaymentService {
         return "thread pool: "+ Thread.currentThread().getName()+" paymentInfo_OK,id: "+id+"\t"+"o(n_n)o`~";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_timeoutHandler", commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="3000")
+    })
     public String paymentInfo_timeout(Integer id){
         try{
-            TimeUnit.SECONDS.sleep(3);
+            TimeUnit.SECONDS.sleep(5);
         }catch (InterruptedException e){
             e.printStackTrace();
         }
         return "thread pool: "+ Thread.currentThread().getName()+" paymentInfo_timeout,id: "+id+"\t"+"o(n_n)o`~";
+    }
+
+    public String paymentInfo_timeoutHandler(Integer id){
+        return "thread pool: "+ Thread.currentThread().getName()+" paymentInfo_timeoutHandler,id: "+id+"\t"+"o(YS)o`~";
     }
 
 }
