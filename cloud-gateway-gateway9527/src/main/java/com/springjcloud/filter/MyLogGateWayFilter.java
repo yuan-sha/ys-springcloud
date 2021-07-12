836a93d5b6a02e762a9725b5a28808e0216e2f90
yuan-sha <yuansha312@gmail.com>
2021-07-12 13:12:51 -0600

    Filter

---
+++ cloud-gateway-gateway9527/src/main/java/com/springjcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,31 @@
+package com.springjcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+       String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+       if(uname==null){
+           exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+           return exchange.getResponse().setComplete();
+       }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
