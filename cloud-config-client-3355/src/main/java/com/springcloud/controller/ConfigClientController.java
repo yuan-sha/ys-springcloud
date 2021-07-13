4e1c4ea3da7249d00c12ed1e33260c70c43b5af1
yuan-sha <yuansha312@gmail.com>
2021-07-13 15:03:13 -0600

    Config Client

---
+++ cloud-config-client-3355/src/main/java/com/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,16 @@
+package com.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+    @Value("${data.env}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
