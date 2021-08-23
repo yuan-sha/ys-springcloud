ab6d6f148ca99d9603725814358c8405ae2f48fa
yuan-sha <yuansha312@gmail.com>
2021-08-23 09:44:57 -0600

    cloud stream receive

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/springcloud/StreamMQ8802.java
@@ -0,0 +1,11 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamMQ8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQ8802.class,args);
+    }
+}
