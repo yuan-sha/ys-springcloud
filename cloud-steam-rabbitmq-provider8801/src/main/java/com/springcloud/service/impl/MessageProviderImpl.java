4a1a94f835e1574c87485f69f41072402616d9db
yuan-sha <yuansha312@gmail.com>
2021-08-23 09:27:40 -0600

    cloud stream send

--- cloud-steam-rabbitmq-provider8801/src/main/java/com/springcloud/service/impl/MessageProviderImpl.java
+++ cloud-steam-rabbitmq-provider8801/src/main/java/com/springcloud/service/impl/MessageProviderImpl.java
@@ -5,20 +5,20 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.MessageChannel;
-
 import javax.annotation.Resource;
 import java.util.UUID;
 
-@EnableBinding(Source.class)
+@EnableBinding(Source.class)    //定义消息的推送管道（Source是spring的）
 public class MessageProviderImpl implements IMessageProvider {
+
     @Resource
-    private MessageChannel ouput;
+    private MessageChannel output;  //消息发送管道
 
     @Override
     public String send() {
         String serial = UUID.randomUUID().toString();
-        ouput.send(MessageBuilder.withPayload(serial).build());
-        System.out.println(serial);
+        output.send(MessageBuilder.withPayload(serial).build());     //MessageBuilder是spring的integration.support.MessageBuilder
+        System.out.println("*******serial: " + serial);
         return null;
     }
 }
