ed55467db6e4426bc4dec43d4ef048ebbcaf8b5f
185388 <ysha@progrexion.com>
2021-09-13 16:58:27 -0600

    @SentinelResource

--- cloudalibaba-sentiner8401/src/main/java/com/springcloud/controller/FlowController.java
+++ cloudalibaba-sentiner8401/src/main/java/com/springcloud/controller/FlowController.java
@@ -1,6 +1,9 @@
 package com.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +17,16 @@ public class FlowController {
     @GetMapping("/getB")
     public String getB(){
         return "BBBBB";
+    }
+
+    @GetMapping("/testhotkey")
+    @SentinelResource(value = "testhotkey", blockHandler = "deal_testhotkey")
+    public String testhotkey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "testhotkey";
+    }
+
+    public String deal_testhotkey(String p1, String p2, BlockException blockException){
+        return "deal_testhotkey";
     }
 }
